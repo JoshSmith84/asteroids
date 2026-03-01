326c30f55373c138c5f7346e69d901b15569470d
JoshSmith84 <jas426@icloud.com>
2026-03-01 15:03:09 -0600

    K: added shoot capability

--- player.py
+++ player.py
@@ -1,10 +1,14 @@
+import pygame
 from circleshape import CircleShape
+from shot import Shot
 from constants import (PLAYER_RADIUS, 
                        LINE_WIDTH, 
                        PLAYER_TURN_SPEED, 
                        PLAYER_SPEED,
+                       SHOT_RADIUS,
+                       PLAYER_SHOOT_SPEED
                        )
-import pygame
+
 
 
 class Player(CircleShape):
@@ -52,3 +56,14 @@ class Player(CircleShape):
             
         if keys[pygame.K_s]:
             self.move(-dt)
+
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+
+    def shoot(self):
+        projectile = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        projectile.velocity = pygame.Vector2(0, 1)
+        projectile.velocity = projectile.velocity.rotate(self.rotation)
+        projectile.velocity *= PLAYER_SHOOT_SPEED
+    
