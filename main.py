d471be9e322ff8df0ba629c4a8f8d28a9aded69b
JoshSmith84 <jas426@icloud.com>
2026-03-01 10:32:31 -0600

    B: logger addition and constants eidt

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
 
 def main():
-    print("Starting Asteroids with pygame version: 2.6.1")
+    print("Starting Asteroids with pygame version: 2.6.1\n"
+    f"Screen width: {SCREEN_WIDTH}\n"
+    f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
